42b099ae0ecc7375dcdfd406172680d54d0b58aa
Chris Woo <cwoo90@gmail.com>
2022-07-27 04:38:01 -0700

    Get predictions for other companies

--- data_functions.py
+++ data_functions.py
@@ -1,4 +1,27 @@
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)   
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :]) 
+        return out
 
 def get_train_data(data, window_size):
     """Get daily training data. Window_size = number of previous days used to predict next days price"""
